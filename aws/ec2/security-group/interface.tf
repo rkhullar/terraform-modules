61b8a1a061b30997abd5167419927c13bf7d0b4a
Rajan Khullar <rkhullar03@gmail.com>
2022-03-26 17:40:22 -0400

    save

---
+++ aws/ec2/security-group/interface.tf
@@ -0,0 +1,90 @@
+# security group resource
+variable "name" {
+  type    = string
+  default = null
+}
+
+variable "name_prefix" {
+  type    = string
+  default = null
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "description" {
+  type    = string
+  default = null
+}
+
+# ingress rules module
+variable "ingress_defaults" {
+  type = object({
+    enable      = bool
+    protocol    = string # tcp | udp | all
+    ports       = list(number)
+    port_ranges = list(string)
+    sources     = list(string)
+  })
+  default = {
+    enable      = true
+    protocol    = "tcp"
+    ports       = []
+    port_ranges = []
+    sources     = []
+  }
+}
+
+variable "ingress" {
+  type = any
+}
+
+# egress rules module
+variable "egress_defaults" {
+  type = object({
+    enable      = bool
+    protocol    = string # tcp | udp | all
+    ports       = list(number)
+    port_ranges = list(string)
+    targets     = list(string)
+  })
+  default = {
+    enable      = true
+    protocol    = "tcp"
+    ports       = []
+    port_ranges = []
+    targets     = []
+  }
+}
+
+variable "egress" {
+  type = any
+}
+
+# custom
+variable "aliases" {
+  type    = map(string)
+  default = {}
+}
+
+# outputs
+output "id" {
+  value = ""
+}
+
+output "arn" {
+  value = ""
+}
+
+output "rules" {
+  value = {
+    ingress = module.ingress-rules.detail
+    egress  = module.egress-rules.detail
+  }
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+## TODO
+- adopt [defaults function][defaults-function] for complex object type variables once [optional attributes][[optional-attributes-experiment]] feature is enabled by default
+
+[defaults-function]: https://www.terraform.io/language/functions/defaults
+[optional-attributes-experiment]: https://www.terraform.io/language/expressions/type-constraints#experimental-optional-object-type-attributes
