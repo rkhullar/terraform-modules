185632c98ff67a6696b8319b9652051b27aa57e6
Rajan Khullar <rkhullar03@gmail.com>
2022-03-27 11:00:37 -0400

    start custom rules module

---
+++ aws/ec2/security-group/custom-rules/interface.tf
@@ -0,0 +1,22 @@
+variable "enable" {
+  type    = bool
+  default = true
+}
+
+variable "rules" {
+  type = list(object({
+    type       = string # ingress | egress
+    protocol   = string # tcp | udp | all
+    port       = optional(number)
+    port_range = optional(string)
+    source     = string
+    target     = string
+  }))
+  default = []
+}
+
+variable "aliases" {
+  type        = map(string)
+  description = "name -> source"
+  default     = {}
+}
