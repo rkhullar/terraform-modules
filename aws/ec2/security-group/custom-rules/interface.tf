978d3b92af40801913cf0c1769f6b68f7b4dba32
Rajan Khullar <rkhullar03@gmail.com>
2022-03-27 14:15:11 -0400

    save

--- aws/ec2/security-group/custom-rules/interface.tf
+++ aws/ec2/security-group/custom-rules/interface.tf
@@ -21,6 +21,10 @@ variable "rules" {
     condition     = length([for rule in var.rules : rule if !contains(["tcp", "udp", "all", "-1"], rule.protocol)]) == 0
     error_message = "Allowed Values: protocol -> {tcp | udp | all}."
   }
+  validation {
+    condition     = length([for rule in var.rules : rule if(rule.port == null && rule.port_range == null) || (rule.port != null && rule.port_range != null)]) == 0
+    error_message = "Mutually Exclusive: [port, port_range]."
+  }
 }
 
 variable "aliases" {
