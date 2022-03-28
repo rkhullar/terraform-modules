5e4bc2bebfd29eae8d50bc65c2d515a4ee03a4f5
Rajan Khullar <rkhullar03@gmail.com>
2022-03-28 16:15:11 -0400

    save

--- aws/ec2/network/data_runtime.tf
+++ aws/ec2/network/data_runtime.tf
@@ -22,3 +22,14 @@ module "data-runtime" {
     targets     = local.rules.data_runtime.egress.targets
   }
 }
+
+module "data-runtime-ingress-linux-runtime" {
+  source      = "../security-group/rule"
+  enable      = local.enable_implicit_rules
+  id          = module.data-runtime.id
+  type        = "ingress"
+  ports       = local.rules.data_runtime.ingress.ports
+  port_ranges = local.rules.data_runtime.ingress.port_ranges
+  sources     = ["linux_runtime"]
+  aliases     = local.default_aliases
+}
--- aws/ec2/network/linux_runtime.tf
+++ aws/ec2/network/linux_runtime.tf
@@ -22,3 +22,25 @@ module "linux-runtime" {
     targets     = local.rules.linux_runtime.egress.targets
   }
 }
+
+module "linux-runtime-ingress-load-balancer" {
+  source      = "../security-group/rule"
+  enable      = local.enable_implicit_rules
+  id          = module.linux-runtime.id
+  type        = "ingress"
+  ports       = local.rules.linux_runtime.ingress.ports
+  port_ranges = local.rules.linux_runtime.ingress.port_ranges
+  sources     = ["load_balancer"]
+  aliases     = local.default_aliases
+}
+
+module "linux-runtime-egress-data-runtime" {
+  source      = "../security-group/rule"
+  enable      = local.enable_implicit_rules
+  id          = module.linux-runtime.id
+  type        = "egress"
+  ports       = local.rules.data_runtime.ingress.ports
+  port_ranges = local.rules.data_runtime.ingress.port_ranges
+  sources     = ["data_runtime"]
+  aliases     = local.default_aliases
+}
