5316ae23dc6997722262dfd0505c8115c8302ad9
Rajan Khullar <rkhullar03@gmail.com>
2022-03-28 14:01:53 -0400

    save

--- aws/ec2/network/load_balancer.tf
+++ aws/ec2/network/load_balancer.tf
@@ -6,11 +6,15 @@ module "load-balancer" {
   vpc_id      = var.vpc_id
   aliases     = local.aliases
 
-  ingress = {
-    enable      = false
-    protocol    = "tcp"
-    ports       = var.rules["load_balancer"].ports
-    port_ranges = var.rules["load_balancer"].port_ranges
-    sources     = var.rules["load_balancer"].sources
-  }
+  #  ingress = {
+  #    enable      = false
+  #    protocol    = "tcp"
+  #    ports       = var.rules["load_balancer"].ports
+  #    port_ranges = var.rules["load_balancer"].port_ranges
+  #    sources     = var.rules["load_balancer"].sources
+  #  }
+}
+
+output "debug-lb" {
+  value = var.rules
 }
