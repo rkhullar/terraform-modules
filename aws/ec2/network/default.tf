c2274c570345f53f923c1b8d180b64b066638c52
Rajan Khullar <rkhullar03@gmail.com>
2022-04-03 11:44:16 -0400

    revisit network module (#9)

--- aws/ec2/network/default.tf
+++ aws/ec2/network/default.tf
@@ -2,9 +2,41 @@ terraform {
   experiments = [module_variable_optional_attrs]
 }
 
+module "security-groups" {
+  for_each    = local.rules
+  source      = "../security-group"
+  name        = local.names[each.key]
+  description = local.descriptions[each.key]
+  tags        = var.tags
+  vpc_id      = var.vpc_id
+  aliases     = local.aliases
+
+  ingress = {
+    enable      = local.enable_rules
+    protocol    = var.egress_protocol
+    ports       = local.rules[each.key].ingress.ports
+    port_ranges = local.rules[each.key].ingress.port_ranges
+    sources     = local.rules[each.key].ingress.sources
+  }
+
+  egress = {
+    enable      = local.enable_rules
+    protocol    = var.egress_protocol
+    ports       = local.rules[each.key].egress.ports
+    port_ranges = local.rules[each.key].egress.port_ranges
+    targets     = local.rules[each.key].egress.targets
+  }
+}
+
+module "security-groups-lookup" {
+  source = "../security-group/lookup"
+  names  = local.names
+  vpc_id = var.vpc_id
+}
+
 module "custom-rules" {
   source  = "../security-group/custom-rules"
-  enable  = var.enable_rules
+  enable  = local.enable_rules
   aliases = local.aliases
   rules   = var.custom_rules
 }
--- aws/ec2/network/locals.tf
+++ aws/ec2/network/locals.tf
@@ -4,6 +4,9 @@ locals {
     linux_runtime = "linux-runtime"
     data_runtime  = "data-runtime"
   })
+}
+
+locals {
   descriptions = defaults(var.descriptions, {
     load_balancer = "security group for load balancer"
     linux_runtime = "security group for linux runtime"
@@ -12,12 +15,9 @@ locals {
 }
 
 locals {
-  aliases = merge(local.default_aliases, var.aliases)
-  default_aliases = {
-    load_balancer = module.load-balancer.id
-    linux_runtime = module.linux-runtime.id
-    data_runtime  = module.data-runtime.id
-  }
+  aliases         = merge(module.security-groups-lookup.output, var.aliases)
+  enable_rules    = var.enable_rules && module.security-groups-lookup.status
+  security_groups = zipmap(keys(module.security-groups), values(module.security-groups)[*].id)
 }
 
 locals {
@@ -30,8 +30,4 @@ locals {
     for _type, leaf in rule : _type => {
       for prop in ["ports", "port_ranges", "sources", "targets"] : prop => coalesce(try(leaf[prop], null), [])
   } } }
-}
-
-locals {
-  enable_implicit_rules = var.enable_rules && var.enable_implicit
 }
