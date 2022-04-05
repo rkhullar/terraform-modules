b2045015675f794fb48853fe1b5c1aa852bc76fa
Rajan Khullar <rkhullar03@gmail.com>
2022-04-05 11:02:52 -0400

    save

--- aws/constructs/vpc-lookup/provider.tf
+++ aws/constructs/vpc-lookup/provider.tf
@@ -2,6 +2,7 @@ terraform {
   # the configuration for this backend will be filled in by terragrunt
   backend "s3" {}
   required_version = "~> 1.1"
+  experiments      = [module_variable_optional_attrs]
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -13,8 +14,4 @@ terraform {
 provider "aws" {
   region              = var.region
   allowed_account_ids = [var.account_id]
-}
-
-terraform {
-  experiments = [module_variable_optional_attrs]
 }
