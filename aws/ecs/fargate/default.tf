7b0f9fcb17d8c94db67b1b72bd37b510462a9887
Rajan Khullar <rkhullar03@gmail.com>
2023-04-23 12:23:13 -0400

    save

--- aws/ecs/fargate/default.tf
+++ aws/ecs/fargate/default.tf
@@ -9,13 +9,18 @@ resource "aws_ecs_cluster" "default" {
 
 resource "aws_ecs_cluster_capacity_providers" "default" {
   cluster_name       = aws_ecs_cluster.default.name
-  capacity_providers = [for item in var.capacity_providers : item["name"]]
+  capacity_providers = [for item in local.capacity_providers : item["name"]]
   dynamic "default_capacity_provider_strategy" {
-    for_each = var.enable_default_strategy ? var.capacity_providers : []
+    for_each = var.enable_default_strategy ? local.capacity_providers : []
     content {
       capacity_provider = default_capacity_provider_strategy.value["name"]
       weight            = default_capacity_provider_strategy.value["weight"]
       base              = default_capacity_provider_strategy.value["base"]
     }
   }
+}
+
+# clean capacity provider names
+locals {
+  capacity_providers = [for item in var.capacity_providers : merge(item, { name = replace(upper(item["name"]), "-", "_") })]
 }
