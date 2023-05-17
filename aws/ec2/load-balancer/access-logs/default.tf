f9ecc99ebfc446867af2590875c56cf5d122f0f8
Rajan Khullar <rkhullar03@gmail.com>
2023-05-17 09:30:59 -0400

    revert access logs change

--- aws/ec2/load-balancer/access-logs/default.tf
+++ aws/ec2/load-balancer/access-logs/default.tf
@@ -1,14 +1,22 @@
-module "default" {
-  source = "../../../s3/bucket"
-  name   = var.name
-  tags = var.tags
-  policy = data.aws_iam_policy_document.default.json
-  lifecycle = [
-    {id=log-expiration, expiration=var.expiration}
-  ]
+resource "aws_s3_bucket" "default" {
+  bucket        = var.name
+  tags          = var.tags
+  force_destroy = true
 }
 
-/*
+resource "aws_s3_bucket_public_access_block" "default" {
+  bucket                  = aws_s3_bucket.default.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_policy" "default" {
+  bucket = aws_s3_bucket.default.id
+  policy = data.aws_iam_policy_document.default.json
+}
+
 resource "aws_s3_bucket_lifecycle_configuration" "default" {
   bucket = aws_s3_bucket.default.id
   rule {
@@ -19,4 +27,3 @@ resource "aws_s3_bucket_lifecycle_configuration" "default" {
     }
   }
 }
-*/
