63908f07309241164aeae6c126ced31f095979e3
Rajan Khullar <rkhullar03@gmail.com>
2023-09-24 10:22:15 -0400

    save

--- aws/lambda/layer/default.tf
+++ aws/lambda/layer/default.tf
@@ -2,6 +2,8 @@ resource "aws_lambda_layer_version" "default" {
   layer_name               = var.name
   compatible_runtimes      = var.runtimes
   compatible_architectures = var.architectures
+  filename                 = data.archive_file.default.output_path
+  #  source_code_hash = data.archive_file.default.output_base64sha256
 }
 
 data "archive_file" "default" {
