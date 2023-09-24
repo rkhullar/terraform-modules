bd18a2dcb24b517289d2f86b7b9b44e6e42e90f9
Rajan Khullar <rkhullar03@gmail.com>
2023-09-24 19:38:59 -0400

    save

--- mongodb/aws/atlas/access/default.tf
+++ mongodb/aws/atlas/access/default.tf
@@ -1,24 +1,24 @@
-resource "mongodbatlas_project_ip_access_list" "default" {
-  for_each   = { for item in var.ingress_sources : item.source => item }
-  project_id = local.atlas_project_id
-  cidr_block = each.value.source
-  comment    = each.value.comment
-}
-
-data "aws_iam_roles" "admin-sso" {
-  path_prefix = "/aws-reserved/sso.amazonaws.com/"
-  name_regex  = ".*AdministratorAccess.*"
+resource "mongodbatlas_database_user" "admin-sso" {
+  for_each           = var.create_admin ? local.aws_admin_roles : {}
+  project_id         = local.project_id
+  auth_database_name = "$external"
+  aws_iam_type       = upper("role")
+  username           = each.value
+  roles {
+    role_name     = "readWriteAnyDatabase"
+    database_name = "admin"
+  }
 }
 
 resource "mongodbatlas_database_user" "default" {
   for_each           = local.aws_role_ids
-  project_id         = local.atlas_project_id
+  project_id         = local.project_id
   auth_database_name = "$external"
   aws_iam_type       = upper("role")
   username           = each.value
   scopes {
     type = upper("cluster")
-    name = var.atlas_cluster
+    name = var.cluster
   }
   dynamic "roles" {
     for_each = local.rules_by_role[each.key]
@@ -28,4 +28,11 @@ resource "mongodbatlas_database_user" "default" {
       collection_name = roles.value.collection
     }
   }
+}
+
+resource "mongodbatlas_project_ip_access_list" "default" {
+  for_each   = { for item in var.ingress_sources : item.source => item }
+  project_id = local.project_id
+  cidr_block = each.value.source
+  comment    = each.value.comment
 }
--- mongodb/aws/atlas/access/interface.tf
+++ mongodb/aws/atlas/access/interface.tf
@@ -1,16 +1,17 @@
-variable "create_atlas_admin" {
+variable "project" {
+  type     = string
+  nullable = false
+}
+
+variable "cluster" {
+  type     = string
+  nullable = false
+}
+
+variable "create_admin" {
   type     = bool
   nullable = false
   default  = false
-}
-
-variable "ingress_sources" {
-  default  = []
-  nullable = false
-  type = set(object({
-    source  = string
-    comment = string
-  }))
 }
 
 variable "access_rules" {
@@ -21,5 +22,26 @@ variable "access_rules" {
     database   = string
     collection = string
     access     = string
+  }))
+}
+
+variable "role_prefix" {
+  type     = string
+  nullable = true
+  default  = null
+}
+
+variable "role_suffix" {
+  type     = string
+  nullable = true
+  default  = null
+}
+
+variable "ingress_sources" {
+  default  = []
+  nullable = false
+  type = set(object({
+    source  = string
+    comment = string
   }))
 }
