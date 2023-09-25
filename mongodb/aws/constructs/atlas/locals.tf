8479928fc52390b578c2de5fbbcbf994bc790414
Rajan Khullar <rkhullar03@gmail.com>
2023-09-24 22:19:01 -0400

    save

--- mongodb/aws/constructs/atlas/locals.tf
+++ mongodb/aws/constructs/atlas/locals.tf
@@ -18,8 +18,8 @@ locals {
 locals {
   standard_connection = compact(local._standard_connection_list)[0]
   _standard_connection_list = [
-    local.create_serverless ? mongodbatlas_serverless_instance.serverless[0].connection_strings_standard_srv[1] : null,
-    local.create_advanced ? mongodbatlas_advanced_cluster.advanced[0].connection : null
+    local.create_serverless ? mongodbatlas_serverless_instance.serverless[0].connection_strings_standard_srv : null,
+    local.create_advanced ? mongodbatlas_advanced_cluster.advanced[0].connection_strings[0].standard_srv : null
   ]
 }
 
